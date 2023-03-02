6c1563363ac1199d360908d37e391b7635c4586c
lfreist <freist@informatik.uni-freiburg.de>
2023-03-02 12:22:19 +0100

    benchmarks

--- benchsuit/run_full.sh
+++ benchsuit/run_full.sh
@@ -3,5 +3,5 @@
 python3 benchsuit/cmdbench/main.py --config benchsuit/suits/xsgrep/ --iterations 1 -o benchsuit/results/xs/cache/$(hostname)
 python3 benchsuit/cmdbench/main.py --config benchsuit/suits/xsgrep/ --iterations 1 -o benchsuit/results/xs/no-cache/$(hostname) --drop-cache "$HOME/drop_ram_caches"
 
-python3 benchsuit/cmdbench/main.py --config benchsuit/search_tools --iterations 1 -o benchsuit/results/search_tools/cache/$(hostname)
-python3 benchsuit/cmdbench/main.py --config benchsuit/search_tools --iterations 1 -o benchsuit/results/search_tools/cache/$(hostname)  --drop-cache "$HOME/drop_ram_caches"
+python3 benchsuit/cmdbench/main.py --config benchsuit/suits/search_tools --iterations 1 -o benchsuit/results/search_tools/cache/$(hostname)
+python3 benchsuit/cmdbench/main.py --config benchsuit/suits/search_tools --iterations 1 -o benchsuit/results/search_tools/cache/$(hostname)  --drop-cache "$HOME/drop_ram_caches"
