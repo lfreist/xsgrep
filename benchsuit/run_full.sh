f5a79e8f853756955e08d8bb51efb549c2b679c5
lfreist <freist@informatik.uni-freiburg.de>
2023-03-02 14:24:45 +0100

    install local

--- benchsuit/run_full.sh
+++ benchsuit/run_full.sh
@@ -4,4 +4,4 @@ python3 benchsuit/cmdbench/main.py --config benchsuit/suits/xsgrep/ --iterations
 python3 benchsuit/cmdbench/main.py --config benchsuit/suits/xsgrep/ --iterations 1 -o benchsuit/results/xs/no-cache/$(hostname) --drop-cache "$HOME/drop_cache"
 
 python3 benchsuit/cmdbench/main.py --config benchsuit/suits/search_tools --iterations 1 -o benchsuit/results/search_tools/cache/$(hostname)
-python3 benchsuit/cmdbench/main.py --config benchsuit/suits/search_tools --iterations 1 -o benchsuit/results/search_tools/cache/$(hostname)  --drop-cache "$HOME/drop_cache"
+python3 benchsuit/cmdbench/main.py --config benchsuit/suits/search_tools --iterations 1 -o benchsuit/results/search_tools/no-cache/$(hostname)  --drop-cache "$HOME/drop_cache"
